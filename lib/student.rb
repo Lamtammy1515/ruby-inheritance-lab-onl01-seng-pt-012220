6d5fe433b6517298e5aed179d17311ad90de9a0d
Tammy Lam <lamtammy1515@gmail.com>
2020-02-26 03:46:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,14 @@
 class Student < User 
   attr_accessor :first_name, :last_name 
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end 
+  
+  def learn
+    
+  end 
 
 end
